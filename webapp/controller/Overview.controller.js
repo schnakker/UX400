f85315b4e1a960954ab7b06d0b5069a08a17cc2f
ux400-c17 UX400-C17 <p003383>
2025-03-05 10:42:22 +0000

    Übung 12

--- webapp/controller/Overview.controller.js
+++ webapp/controller/Overview.controller.js
@@ -1,14 +1,38 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/syncStyleClass"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, syncStyleClass) {
         "use strict";
 
         return Controller.extend("sap.training.exc.controller.Overview", {
+              
+               onSave: function () {
+//Fragment wird nur instanziert, wenn es noch nicht existiert.                    
+                if (!this.pDialog) {
+                  this.pDialog = this.loadFragment({
+                    name: "sap.training.exc.view.Dialog"
+                  }).then(function (oDialog) {
 
+//Übergeben der Conten Density Informationen an den Dialog.                    
+              syncStyleClass(this.getOwnerComponent().getContentDensityClass(), 
+              this.getView(), oDialog);
+                    return oDialog;
+                  }.bind(this));
+                }
+                this.pDialog.then(function (oDialog) {
+                  oDialog.open();
+                });
+               },
+
+//Schließen des Pop-Ups.               
+               onCloseDialog: function () {
+//byId = Id im Dialog.fragment.xml.
+                this.byId("dialog").close();
+               }
 
         });
     });
