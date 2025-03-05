28ee8ec0e41e145e609f4079b9fbfcbf975756de
ux400-c17 UX400-C17 <p003383>
2025-03-05 15:46:11 +0000

    Übung 17

---
+++ webapp/model/formatter.js
@@ -0,0 +1,17 @@
+sap.ui.define([], function () {
+    "use strict";
+    return {
+        classText: function (sClass) {
+            switch (sClass) {
+                case "C":
+                    return "Business Class";
+                case "Y":
+                    return "Economy Class";
+                case "F":
+                    return "First Class";
+                default:
+                    return sClass;
+            }
+        }
+    };
+});
