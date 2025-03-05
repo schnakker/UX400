377c5d6f7ea9b31ebe63ac6f319bc90355226142
ux400-c17 UX400-C17 <p003383>
2025-03-05 12:37:27 +0000

    Übung 13

--- webapp/controller/Overview.controller.js
+++ webapp/controller/Overview.controller.js
@@ -1,15 +1,27 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/core/syncStyleClass"
+    "sap/ui/core/syncStyleClass",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, syncStyleClass) {
+    function (Controller, syncStyleClass, JSONModel) {
         "use strict";
 
         return Controller.extend("sap.training.exc.controller.Overview", {
               
+          onInit: function () {
+//neue Variable/Object als JSONModel            
+            var oModel = new JSONModel();
+//Die Struktur des Modells wird zur Laufzeit vergeben und gefüllt. Man kann es auch vorher mit Daten versorgen:
+//oModel.setData({
+//  "CustomerName": "Anna Muster",
+//  "Street": "Anna Lyse"
+//})
+            this.getView().setModel(oModel, "customer"); //JSON Object wird dem Programm zu gewiesen. Das Modell bekommt Programmintern den Namen "customer".
+           },
+          
                onSave: function () {
 //Fragment wird nur instanziert, wenn es noch nicht existiert.                    
                 if (!this.pDialog) {
@@ -18,10 +30,10 @@ sap.ui.define([
                   }).then(function (oDialog) {
 
 //Übergeben der Conten Density Informationen an den Dialog.                    
-              syncStyleClass(this.getOwnerComponent().getContentDensityClass(), 
-              this.getView(), oDialog);
-                    return oDialog;
-                  }.bind(this));
+                  syncStyleClass(this.getOwnerComponent().getContentDensityClass(), 
+                  this.getView(), oDialog);
+                        return oDialog;
+                      }.bind(this));
                 }
                 this.pDialog.then(function (oDialog) {
                   oDialog.open();
