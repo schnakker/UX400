01ae396437b99d0928bfb8e8230c67348000aefb
ux400-c17 UX400-C17 <p003383>
2025-03-06 14:57:46 +0000

    Übung 21 Erweiterung

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -1,17 +1,19 @@
-sap.ui.define([], function () {
+sap.ui.define(["sap/base/i18n/ResourceBundle"], function (ResourceBundle) {
     "use strict";
     return {
+
         classText: function (sClass) {
+            var oResourceBundle = ResourceBundle.create({ url: "i18n/i18n.properties" });
             switch (sClass) {
-                case "C":
-                    return "Business Class";
-                case "Y":
-                    return "Economy Class";
-                case "F":
-                    return "First Class";
-                default:
-                    return sClass;
+              case "C":
+                return oResourceBundle.getText("flightClassC");
+              case "Y":
+                return oResourceBundle.getText("flightClassY");
+              case "F":
+                return oResourceBundle.getText("flightClassF");
+              default:
+                return sClass;
             }
-        }
+           }
     };
 });
